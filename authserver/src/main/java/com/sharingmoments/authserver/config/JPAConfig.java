4773639ff35c833fd50a0c99829bd115629a1ab6
Jan-Philipp Schack <jpschack@gmail.com>
2016-09-20 18:47:53 +1000

    fixed issues with jdbc dataSource

--- authserver/src/main/java/com/sharingmoments/authserver/config/JPAConfig.java
+++ authserver/src/main/java/com/sharingmoments/authserver/config/JPAConfig.java
@@ -1,17 +1,63 @@
 package com.sharingmoments.authserver.config;
 
-import org.springframework.boot.orm.jpa.EntityScan;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.sharingmoments.resource.persistence.service.AwsS3ServiceImpl;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.sharingmoments.resource.persistence.doa")
-@ComponentScan(basePackages = { "com.sharingmoments.resource.persistence.service", "com.sharingmoments.resource.security" }, excludeFilters = @ComponentScan.Filter(value = AwsS3ServiceImpl.class, type = FilterType.ASSIGNABLE_TYPE))
-@EntityScan({"com.sharingmoments.resource.persistence.model"})
+@ComponentScan(basePackages = { "com.sharingmoments.resource.persistence.service", "com.sharingmoments.resource.security" }, excludeFilters = { @ComponentScan.Filter(value = AwsS3ServiceImpl.class, type = FilterType.ASSIGNABLE_TYPE) })
 @EnableTransactionManagement
-public class JPAConfig {}
+public class JPAConfig {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource);
+		em.setPackagesToScan(new String[] { "com.sharingmoments.resource.persistence.model" });
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.put("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		return properties;
+	}
+}
--- resource/src/main/java/com/sharingmoments/resource/config/JPAConfig.java
+++ resource/src/main/java/com/sharingmoments/resource/config/JPAConfig.java
@@ -1,15 +1,61 @@
 package com.sharingmoments.resource.config;
 
-import org.springframework.boot.orm.jpa.EntityScan;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.sharingmoments.resource.persistence.doa")
 @ComponentScan(basePackages = { "com.sharingmoments.resource.persistence.service", "com.sharingmoments.resource.security" })
-@EntityScan({"com.sharingmoments.resource.persistence.model"})
 @EnableTransactionManagement
-public class JPAConfig {}
+public class JPAConfig {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource);
+		em.setPackagesToScan(new String[] { "com.sharingmoments.resource.persistence.model" });
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.put("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		return properties;
+	}
+}
